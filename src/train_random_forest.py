69d43947a03d82e21105162893694f157db4a143
Dhruv Mistry <dhruvmistry3377@gmail.com>
2026-06-04 04:51:44 +0530

    Random Forest

---
+++ src/train_random_forest.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    classification_report
+)
+
+df = pd.read_csv("data/creditcard.csv")
+
+df = df.drop_duplicates()
+
+X = df.drop("Class", axis=1)
+y = df["Class"]
+
+X_train, X_test, y_train, y_test = train_test_split(  # Train-Test Split
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+scaler = StandardScaler()                  # Initialize scaler
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = RandomForestClassifier(            # Initialize Random Forest
+    n_estimators=100,
+    random_state=42,
+    n_jobs=-1
+)
+
+model.fit(X_train, y_train)                # TRAINING
+
+y_pred = model.predict(X_test)             # PREDICTIONS
+
+print("\nAccuracy:")                       # EVALUATION
+print(accuracy_score(y_test, y_pred))
+
+print("\nPrecision:")
+print(precision_score(y_test, y_pred))
+
+print("\nRecall:")
+print(recall_score(y_test, y_pred))
+
+print("\nF1 Score:")
+print(f1_score(y_test, y_pred))
+
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
